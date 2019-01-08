0733c8b494d1767d2f98362925a1fd2217d13db3
Richard Skogeby <richard.skogeby@outlook.com>
2019-01-08 16:24:13 +0000

    Put class Gaussian in toolbox instead of in this file

--- merlin/equalisation/phase_1_plots/plot_gaussians.py
+++ merlin/equalisation/phase_1_plots/plot_gaussians.py
@@ -4,37 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from toolbox.plot.default_style import apply_default_plot_style
+from toolbox.gaussian import Gaus
 
 apply_default_plot_style()
-
-
-class Gaus:
-    """Class for generating a Gaussian on the form Gaus.equation() with
-        norm 1 times area, mean at mu and standard deviation of sigma.
-        x is a numpy.ndarray of arbitrary size, respective y values for
-        the generated gaussian can be accessed through Gaus.y.
-    """
-
-    def __init__(self, x, area: float, mu: float, sigma: float):
-        self.x = x
-        self.area = area
-        self.mu = mu
-        self.sigma = sigma
-        self.y = self.area/np.sqrt(2*np.pi*self.sigma**2) * \
-            np.exp(-(self.x-self.mu)**2/(2*self.sigma**2))
-
-    def equation(self):
-        return 'f(x)=area/sqrt(2 pi sigma^2) exp(-(x-mu)^2/(2 sigma^2))'
-
-    def generate(self):
-        self.y = self.area/np.sqrt(2*np.pi*self.sigma**2) * \
-            np.exp(-(self.x-self.mu)**2/(2*self.sigma**2))
 
 
 def main():
     """Plots and formats a Gaussian. Formatting is done almost independently
     from what's plotted. As long as xs, func_1 and func_2 are properly filled
-    in aspet ratios should be kept and text should stay in place.    
+    in. Aspect ratios should remain constant and text should stay in place.    
     """
 
     # Function to be plotted, this is the only hard-coded part
